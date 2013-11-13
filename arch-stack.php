0bb162536814cbfc40e9cefd408a5d5f14e6e779
Derek Chafin <infomaniac50@gmail.com>
2013-11-13 16:05:58 -0600

    Renamed first script file to start.sh to keep it consistent with my other bootstrap scripts.

---
+++ arch-stack.php
@@ -0,0 +1,11 @@
+<?php
+
+header("Content-Type: text/plain");
+
+function println($string) {
+	echo $string;
+	echo "\n";
+}
+
+println('curl -Ss http://coding-squared.com/arch-stack.tar.gz | tar -xz --no-same-owner --no-same-permissions -f -');
+println('echo "Please run \'bash start.sh\'"');
