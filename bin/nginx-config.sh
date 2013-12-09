6c9233a5d069bddc7d7fd7cd29432c909fa082d5
Derek Chafin <infomaniac50@gmail.com>
2013-12-09 13:49:50 -0600

    Automatically change server_name variable to hostname.

--- bin/nginx-config.sh
+++ bin/nginx-config.sh
@@ -8,7 +8,13 @@
 
 
 CONFIGFILE="/etc/nginx/nginx.conf"
+SITESCONFIG="/etc/nginx/sites.conf"
 
 cp "$CONFIGFILE" "$CONFIGFILE.bak"
+cp "$SITESCONFIG" "$SITESCONFIG.bak"
+
 sed -e'/^\t*\s*#gzip  on;/a \ \ \ \ include /etc/nginx/sites.conf;' "$CONFIGFILE" > "$CONFIGFILE.new"
 mv "$CONFIGFILE.new" "$CONFIGFILE"
+
+sed 's:^\s*\t*server_name\s*arch-vaio;:a\ \ server_name\ '`hostname`';:' "$SITESCONFIG" > "$SITESCONFIG.new"
+mv "$SITESCONFIG.new" "$SITESCONFIG"
